260833149718d1d545cac2874d669c025ea7eebd
dlhsiao <danalhsiao@gmail.com>
2025-01-25 12:13:54 -0500

    Scene Understanding spawns one object on table

---
+++ Assets/Scripts/Logs.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Logs : MonoBehaviour
+{
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+
+  public void SceneLoad() {
+    Debug.Log("Called OnSceneLoad().");
+  }
+
+  public void RoomCreate() {
+    Debug.Log("Called OnRoomCreate().");
+  }
+}
