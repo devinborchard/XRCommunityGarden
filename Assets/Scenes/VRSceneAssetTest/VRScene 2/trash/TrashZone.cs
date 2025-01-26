d79d4102a434889526e51ad9ee512482047b5c07
dlhsiao <danalhsiao@gmail.com>
2025-01-25 19:02:17 -0500

    move sound and trash checking to trash can

--- Assets/Scenes/VRSceneAssetTest/VRScene 2/trash/TrashZone.cs
+++ Assets/Scenes/VRSceneAssetTest/VRScene 2/trash/TrashZone.cs
@@ -8,9 +8,24 @@ public class TrashZone : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    // // Update is called once per frame
+    // void Update()
+    // {
         
+    // }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("trash"))
+        {
+            GameObject[] trash = GameObject.FindGameObjectsWithTag("trash");
+            if (trash.Length - 1 == 0) {
+              Debug.Log("All Trash trashed");
+            }
+
+            AudioSource audio = gameObject.GetComponent<AudioSource>();
+            audio.Play();
+        }
     }
 }
